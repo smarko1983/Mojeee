3ea489d65a8b1d3ef78139b00ed3b76b04cfd6a2
smarko1983 <smarko1983@yahoo.com>
2021-03-17 23:54:17 +0800

    Update Rock Paper Scissors - My Implementation.py

--- Rock Paper Scissors - My Implementation.py
+++ Rock Paper Scissors - My Implementation.py
@@ -152,5 +152,100 @@ rock_paper_scissors_game()
 
 
 
+# 3rd version - I added the total number of wins, loses, draws, and games
+
+import random
+
+
+def rock_paper_scissors_game():
+
+    wins = 0
+    loses = 0
+    draws = 0
+    games = 0
+
+    while True:
+        def choose():
+            print("Welcome to the rock paper scissors game!")
+            letters = ["r", "p", "s"]
+            me = input("Please enter a letter:  ")
+            computer = random.choice(letters)
+            print("the computer choose: ", computer)
+            return play(me, computer)
+
+        def play(me, computer):
+            nonlocal games, loses, draws, wins
+            if (me == "r" and computer == "s") or (me == "s" and computer == "p") or (me == "p" and computer == "r"):
+                games = games + 1
+                wins = wins + 1
+                return winner()
+            elif (me == "r" and computer == "r") or (me == "s" and computer == "s") or (me == "p" and computer == "p"):
+                games = games + 1
+                draws = draws + 1
+                return draw()
+            else:
+                games = games + 1
+                loses = loses + 1
+                return looser()
+
+        def winner():
+            print(r'''
+         |       |
+        (| SFAfS |)
+         |  #X   |
+          \     /
+           `---'
+           _|_|_''')
+
+        def looser():
+            print(r'''
+        .     .-""""""-.
+           .'  \\  //  '.
+          /   O      O   \
+         :                :
+         |                |  angry...hate loosing!
+         :       __       :
+          \  .-"`  `"-.  /
+           '.          .'
+             '-......-'
+        ''')
+
+        def draw():
+            print(r'''
+        .----------------.  .----------------.  .----------------.  .----------------.
+        | .--------------. || .--------------. || .--------------. || .--------------. |
+        | |  ________    | || |  _______     | || |      __      | || | _____  _____ | |
+        | | |_   ___ `.  | || | |_   __ \    | || |     /  \     | || ||_   _||_   _|| |
+        | |   | |   `. \ | || |   | |__) |   | || |    / /\ \    | || |  | | /\ | |  | |
+        | |   | |    | | | || |   |  __ /    | || |   / ____ \   | || |  | |/  \| |  | |
+        | |  _| |___.' / | || |  _| |  \ \_  | || | _/ /    \ \_ | || |  |   /\   |  | |
+        | | |________.'  | || | |____| |___| | || ||____|  |____|| || |  |__/  \__|  | |
+        | |              | || |              | || |              | || |              | |
+        | '--------------' || '--------------' || '--------------' || '--------------' |
+         '----------------'  '----------------'  '----------------'  '----------------'
+                ''')
+
+        # starting a game  - calling a choose function
+
+        choose()
+        start = input("If you want to continue, write yes. ").lower()
+        if start != "yes":
+            print("bye byeee")
+            break
+    print("Total games: ", games)
+    print("Wins: ",wins)
+    print("Loses: ", loses)
+    print("Draws: ", draws)
+
+
+
+
+rock_paper_scissors_game( )
+
+
+
+
+
+
 
 
